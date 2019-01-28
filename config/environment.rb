ba3d733f1587ab967ae06f3add33a513916588f5
Jacob Feldman <jacobgfeld@gmail.com>
2019-01-28 14:34:24 -0500

    Adds authentication and a few basic data requests

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'bundler'
+require 'require_all'
+require 'httpclient'
+require 'jsonclient'
+
+require_all 'lib'
+include UnifiApi
--- lib/unifi_client.rb
+++ lib/unifi_client.rb
@@ -1,6 +1,36 @@
 module UnifiApi
   class UnifiClient
-    def login(controller_user, controller_password, controller_url)
+    attr_reader :controller_user, :controller_password, :controller_url, :site_id
+
+    def initialize(controller_user, controller_password, controller_url, site_id=nil)
+      @session = JSONClient.new(default_header: {"User-Agent" => "unifi_api"})
+
+      @controller_user = controller_user
+      @controller_password = controller_password
+      @controller_url = controller_url
+      @site_id = site_id
     end
+    
+    def login
+      login_data = {
+        'username' => @controller_user,
+        'password' => @controller_password
+      }
+      
+      @session.post("#{@controller_url}/api/login", login_data)
+    end
+
+    def sites
+      @session.get("#{@controller_url}/api/self/sites")
+    end
+
+    def statSites
+      @session.get("#{@controller_url}/api/stat/sites")      
+    end
+
+    def statAdmin
+      @session.get("#{@controller_url}/api/stat/admin")
+    end
+
   end
 end
