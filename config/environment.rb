9e6fb8453379abfd62c9e4a062cdd255310d0df9
Jacob Feldman <jacobgfeld@gmail.com>
2019-01-28 15:47:12 -0500

    Changes enviroment importing order

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,11 @@ require 'require_all'
 require 'httpclient'
 require 'jsonclient'
 
+require './lib/unifi_rest.rb'
+require './lib/unifi_stat.rb'
+require './lib/unifi_self.rb'
+require './lib/s/s_cmd_stamgr.rb'
+
 require_all 'lib'
+
 include UnifiApi
