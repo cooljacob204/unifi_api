2bbb0674941cd0a4f30f79c6c9a2f39858a90c55
Jacob Feldman <jacobgfeld@gmail.com>
2019-01-29 18:19:25 -0500

    Adds more client functionality

--- lib/s/s_cmd_stamgr.rb
+++ lib/s/s_cmd_stamgr.rb
@@ -4,7 +4,7 @@ module UnifiApi
   module S
     module CMD
       module STAMGR
-        def guest_authorize(mac, minutes=60, up=nil, down=nil, mbytes=nil, ap_mac=nil)
+        def authorize_guest(mac, minutes=60, up=nil, down=nil, mbytes=nil, ap_mac=nil)
           return false if !site_id
 
           body = {
@@ -17,6 +17,58 @@ module UnifiApi
           body['mbytes'] = mbytes if mbytes
           body['ap_mac'] = ap_mac if ap_mac
 
+          @session.post("#{@controller_url}/api/s/#{@site_id}/cmd/stamgr", body)
+        end
+
+        def unauthorize_guest(mac)
+          return false if !site_id
+
+          body = {
+            'cmd' => 'unauthorize-guest',
+            'mac' => mac
+          }
+
+          @session.post("#{@controller_url}/api/s/#{@site_id}/cmd/stamgr", body)
+        end
+
+        def reconnect_sta(mac)
+          return false if !site_id
+
+          body = {
+            'cmd' => 'kick-sta',
+            'mac' => mac
+          }
+
+          @session.post("#{@controller_url}/api/s/#{@site_id}/cmd/stamgr", body)
+        end
+
+        def block_sta
+          return false if !site_id
+
+          body = {
+            'cmd' => 'block-sta',
+            'mac' => mac
+          }
+          @session.post("#{@controller_url}/api/s/#{@site_id}/cmd/stamgr", body)
+        end
+
+        def unblock_sta
+          return false if !site_id
+
+          body = {
+            'cmd' => 'unblock-sta',
+            'mac' => mac
+          }
+          @session.post("#{@controller_url}/api/s/#{@site_id}/cmd/stamgr", body)
+        end
+
+        def forget_sta
+          return false if !site_id
+
+          body = {
+            'cmd' => 'forget-sta',
+            'mac' => mac
+          }
           @session.post("#{@controller_url}/api/s/#{@site_id}/cmd/stamgr", body)
         end
       end
