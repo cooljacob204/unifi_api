8459170bd2a9f059500d2c91b63fb4b9deb5c0c8
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-19 17:13:36 -0500

    Fixes requirements

--- lib/unifi_api.rb
+++ lib/unifi_api.rb
@@ -1,6 +1,15 @@
-require "unifi_api/version"
+require 'httpclient'
+require 'jsonclient'
+
+require 'unifi_api/version'
+require 'unifi_api/unifi_rest'
+require 'unifi_api/unifi_stat'
+require 'unifi_api/unifi_self'
+require 'unifi_api/s/s_cmd_stamgr'
+
+require 'unifi_api/unifi_client'
 
 module UnifiApi
   class Error < StandardError; end
-  # Your code goes here...
+
 end
--- lib/unifi_api/version.rb
+++ lib/unifi_api/version.rb
@@ -1,3 +1,3 @@
 module UnifiApi
-  VERSION = "0.1.0"
+  VERSION = "0.1.19"
 end
