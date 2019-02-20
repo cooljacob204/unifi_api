d976ce4b64829e3f6b63d3961d816d32f6495da0
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-20 12:44:43 -0500

    Updates file names to remove repetition in file naming

--- lib/unifi_api.rb
+++ lib/unifi_api.rb
@@ -2,12 +2,12 @@ require 'httpclient'
 require 'jsonclient'
 
 require 'unifi_api/version'
-require 'unifi_api/unifi_rest'
-require 'unifi_api/unifi_stat'
-require 'unifi_api/unifi_self'
+require 'unifi_api/rest'
+require 'unifi_api/stat'
+require 'unifi_api/self'
 require 'unifi_api/s/s_cmd_stamgr'
 
-require 'unifi_api/unifi_client'
+require 'unifi_api/client'
 
 module UnifiApi
   class Error < StandardError; end
