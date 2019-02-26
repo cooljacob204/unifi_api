6afb2882cf3ea9f0eb873a719521eab475f1eae2
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-26 15:09:56 -0500

    Turns stat into a class stats to reduce dependencies

--- lib/unifi_api/stat.rb
+++ lib/unifi_api/stat.rb
@@ -1,17 +1,19 @@
 module UnifiApi
-  module Stat
-    def stat_sites
-      resp = @session.get("#{@controller_url}/api/stat/sites")
+  class Stats
+    class << self
+      def stat_sites(session, controller_url)
+        resp = session.get("#{controller_url}/api/stat/sites")
 
-      return false unless resp.status_code == 200
-      resp
-    end
+        return false unless resp.status_code == 200
+        resp
+      end
 
-    def stat_admin
-      resp = @session.get("#{@controller_url}/api/stat/admin")
+      def stat_admin(session, controller_url)
+        resp = session.get("#{controller_url}/api/stat/admin")
 
-      return false unless resp.status_code == 200
-      resp
+        return false unless resp.status_code == 200
+        resp
+      end
     end
   end
 end
