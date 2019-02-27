196c4c5d6a355e54843f5512273744821e0de4a9
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-27 14:23:05 -0500

    Changes post method to have the domain alread known

--- lib/unifi_api/session.rb
+++ lib/unifi_api/session.rb
@@ -3,12 +3,23 @@ module UnifiApi
     attr_reader :session
 
     def initialize(default_header: {"User-Agent" => "unifi_api"}, username:, password:, url:, **args)
-      super
+      super(default_header: default_header)
       @username = username
       @password = password
       @url = url
       @logged_in = false
     end
+
+    def post(uri, *args, &block) # overwrites JSONclient post
+      uri = "#{@url}/#{uri}"
+      request(:post, uri, argument_to_hash_for_json(args), &block)
+    end
+
+    def get(uri, *args, &block) # overwrites JSONclient get
+      uri = "#{@url}/#{uri}"
+      request(:get, uri, argument_to_hash(args, :query, :header, :follow_redirect), &block)
+    end
+
 
     def login
       return true if logged_in?
@@ -18,7 +29,7 @@ module UnifiApi
         'password' => @password
       }
       
-      resp = post("#{@url}/api/login", login_data)
+      resp = post("api/login", login_data)
       
       raise ArgumentError if resp.status_code == 400
       return false unless resp.status_code == 200
@@ -27,10 +38,9 @@ module UnifiApi
     end
 
     def logout
-      resp = post("#{@url}/api/logout", {})
+      resp = post("api/logout", {})
 
       return false unless resp.status_code == 200
-
       @logged_in = false
       true
     end
