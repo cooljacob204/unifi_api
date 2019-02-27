250ff7af1b2113029b4fe6c415e6d221f943952f
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-27 14:24:34 -0500

    updates instance variable name and adds login/logout tests

--- spec/unifi_api/session_spec.rb
+++ spec/unifi_api/session_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe UnifiApi::Session do
   before(:all) do
-    @unifi = UnifiApi::Session.new(username: ENV['TEST_USERNAME'], password: ENV['TEST_PASSWORD'], url: ENV['TEST_URL'])
-    @unifi.login
+    @session = UnifiApi::Session.new(username: ENV['TEST_USERNAME'], password: ENV['TEST_PASSWORD'], url: ENV['TEST_URL'])
+    @session.login
   end
 
   describe 'initialize' do
@@ -19,8 +19,13 @@ describe UnifiApi::Session do
       expect{UnifiApi::Session.new(username: 'bad-username', password: 'bad-password', url: ENV['TEST_URL']).login}.to raise_error(ArgumentError)
     end
 
-    it 'logins in with good credentials' do
-      expect(@unifi.logged_in?).to equal true
+    it 'can login with good credentials' do
+      expect(@session.logged_in?).to equal true
+    end
+
+    it 'can logout' do
+      @session.logout
+      expect(@session.logged_in?).to equal false
     end
   end
 end
